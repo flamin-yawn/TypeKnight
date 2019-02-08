647e4550d976fd430add46500dac147418f62cbd
William Lan <wlan76@yahoo.com>
2019-02-08 13:37:48 -0800

    update menu

--- Word Typing Game/Assets/MainMenu.cs
+++ Word Typing Game/Assets/MainMenu.cs
@@ -9,9 +9,4 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
-    public void QuickGame()
-    {
-        SceneManager.LoadScene(0);
-    }
 }
