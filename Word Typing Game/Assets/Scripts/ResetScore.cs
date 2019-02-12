d2344b3d4ef646ec75be74a15052b05e64dd1971
William Lan <wlan76@yahoo.com>
2019-02-12 11:57:02 -0800

    update games add score

---
+++ Word Typing Game/Assets/Scripts/ResetScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetScore : MonoBehaviour
+{
+    public static int scoreValue = 0;
+    Text score; 
+    
+    void Update()
+    {
+        score.text = "Score: " + scoreValue;
+    }
+}
